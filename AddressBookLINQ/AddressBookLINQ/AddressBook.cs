56f5ae3dace098d8d553fcefdfac2343ff2667fb
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-06-19 22:23:25 +0530

    [Raj]Add: Create Address Book Table with First,last names,address,city,state,zip,phone number and email

--- AddressBookLINQ/AddressBookLINQ/AddressBook.cs
+++ AddressBookLINQ/AddressBookLINQ/AddressBook.cs
@@ -59,6 +59,57 @@ namespace AddressBookLINQ
             dataTable.Columns.Add(dtColumn);
 
         }
-       
+        public int AddValues()
+        {
+            CreateDataTable();
+            Contact contact = new Contact();
+            Contact contacts = new Contact();
+            contact.ID = 1;
+            contact.FirstName = "Rajvardhan";
+            contact.LastName = "Singh";
+            contact.PhoneNumber = 8439560765;
+            contact.Email = "rajvardhan.2627@gmail.com";
+            contact.Address = "Himalayan Colony";
+            contact.City = "Najibabad";
+            contact.State = "UP";
+            contact.zip = 246763;
+            InsertintoDataTable(contact);
+
+            contacts.ID = 2;
+            contacts.FirstName = "Kshitij";
+            contacts.LastName = "Puri";
+            contacts.PhoneNumber = 7012657852;
+            contacts.Email = "puri.kshitij@gmail.com";
+            contacts.Address = "IIT Road";
+            contacts.City = "Roorke";
+            contacts.State = "UK";
+            contacts.zip = 247001;
+            InsertintoDataTable(contacts);
+
+            return dataTable.Rows.Count;
+        }
+
+        public void InsertintoDataTable(Contact contact)
+        {
+            DataRow dtRow = dataTable.NewRow();
+            dtRow["ID"] = contact.ID;
+            dtRow["FirstName"] = contact.FirstName;
+            dtRow["LastName"] = contact.LastName;
+            dtRow["Address"] = contact.Address;
+            dtRow["City"] = contact.City;
+            dtRow["State"] = contact.State;
+            dtRow["Zip"] = contact.zip;
+            dtRow["PhoneNumber"] = contact.PhoneNumber;
+            dtRow["Email"] = contact.Email;
+            dataTable.Rows.Add(dtRow);
+
+        }
+        public void Display()
+        {
+            foreach (DataRow dtRows in dataTable.Rows)
+            {
+                Console.WriteLine(" ID: {0} \n First Name: {1} \n Last Name: {2} \n Address: {3} \n City: {4} \n State: {5} \n Zip: {6} \n Phone Number: {7} \n Email: {8} \n", dtRows["ID"], dtRows["FirstName"], dtRows["LastName"], dtRows["Address"], dtRows["City"], dtRows["State"], dtRows["Zip"], dtRows["PhoneNumber"], dtRows["Email"]);
+            }
+        }
     }
 }
