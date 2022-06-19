24cfb5898e1898f15e4e7b875589b92ac9c585e4
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-06-19 22:48:45 +0530

    [Raj]Add: Delete a contact using their name

--- AddressBookLINQ/AddressBookLINQ/AddressBook.cs
+++ AddressBookLINQ/AddressBookLINQ/AddressBook.cs
@@ -126,5 +126,22 @@ namespace AddressBookLINQ
                 return 0;
             }
         }
+
+        public int DeleteRowInDataTable(string FirstName)
+        {
+            AddValues();
+            var modifiedList = (from ContactList in dataTable.AsEnumerable() where ContactList.Field<string>("FirstName") == FirstName select ContactList).FirstOrDefault();
+            if (modifiedList != null)
+            {
+                modifiedList.Delete();
+                Console.WriteLine("After Deletion");
+                Display();
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
